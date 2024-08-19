54be75ff700272525ba80d9ceae7c038d685d84d
Arpit Mishra <arpitbbsrboy@gmail.com>
2024-08-19 22:51:19 +0530

    added sample flipbook

--- src/Map.jsx
+++ src/Map.jsx
@@ -102,22 +102,27 @@ const Map = () => {
         {
           name: 'New Delhi',
           coordinates: [77.2090, 28.6139],
+          link: 'https://heyzine.com/flip-book/d8c1c5c1e9.html#page/4',
         },
         {
           name: 'Mumbai',
           coordinates: [72.8777, 19.0759],
+          link: 'https://en.wikipedia.org/wiki/Mumbai',
         },
         {
           name: 'Bengaluru',
           coordinates: [77.5946, 12.9716],
+          link: 'https://heyzine.com/flip-book/d8c1c5c1e9.html#page/6',
         },
         {
           name: 'Hyderabad',
           coordinates: [78.4867, 17.3850],
+          link: 'https://en.wikipedia.org/wiki/Hyderabad',
         },
         {
           name: 'Chennai',
           coordinates: [80.2707, 13.0827],
+          link: 'https://en.wikipedia.org/wiki/Chennai',
         },
       ];
 
@@ -128,33 +133,33 @@ const Map = () => {
         })
         .setLngLat(location.coordinates)
         .setPopup(new maplibregl.Popup({ offset: 25 }) // Offset the popup by 25px
-          .setHTML(`<a href="https://www.google.com/maps/search/?api=1&query=${location.name}" target="_blank">${location.name}</a>`))
+          .setHTML(`<a href="${location.link}" target="_blank">${location.name}</a>`))
         .addTo(map);
+      });
     });
-  });
 
-  map.on('click', (e) => {
-    console.log('Map clicked at', e.lngLat);
-  });
+    map.on('click', (e) => {
+      console.log('Map clicked at', e.lngLat);
+    });
 
-  return () => map.remove();
-}, [mapContainerRef]);
+    return () => map.remove();
+  }, [mapContainerRef]);
 
-return (
-  <div
-    ref={mapContainerRef}
-    style={{
-      width: '95vw',
-      height: '100vh',
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      bottom: 0,
-      right: 0,
-      margin: 'auto',
-    }}
-  />
-);
+  return (
+    <div
+      ref={mapContainerRef}
+      style={{
+        width: '95vw',
+        height: '100vh',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        margin: 'auto',
+      }}
+    />
+  );
 };
 
 export default Map;
